1dd71cdbb6bc27b6ce9f8968e42b2992c39187ea
tawachan <tawachan39@gmail.com>
2022-02-07 15:10:48 +0900

    fix

--- sb2md.py
+++ sb2md.py
@@ -1,7 +1,12 @@
+from dataclasses import replace
 import json
 import sys
 import re
 import os
+from this import s
+import requests
+from datetime import datetime, date, time
+import pytz
 
 
 def main():
@@ -14,14 +19,17 @@ def main():
         for p in sb['pages']:
             title = p['title']
             lines = p['lines']
+            created_at = datetime.fromtimestamp(p['created'], tz=pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S')
+            updated_at = datetime.fromtimestamp(p['updated'], tz=pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S')
             is_in_codeblock = False
             is_in_table = False
             row = -1
             title = title.replace('/', '_')
             with open(f'{outdir}{title}.md', 'w', encoding='utf-8') as fw:
+                fw.write('作成日時: ' + created_at + '\n' + '更新日時: ' + updated_at + '\n')
                 for i, l in enumerate(lines):
                     if i == 0:
-                        l = '# ' + l
+                        l = '\n' # 1行目は空行、最初のタイトルは無視
                     else:
                         # 複数行コードブロックの処理
                         if l.startswith('code:'):
@@ -56,6 +64,7 @@ def main():
 
 
 def convert(l: str) -> str:
+    l = escape_customized_decorator(l)
     l = escape_hash_tag(l)
     l = convert_list(l)
     l = convert_bold(l)
@@ -63,12 +72,17 @@ def convert(l: str) -> str:
     l = convert_link(l)
     return l
 
+def escape_customized_decorator(l: str) -> str:
+    for m in re.finditer(r'\[\*+\.+\s.+?\]', ignore_code(l)):
+        l = l.replace(m.group(0), m.group(0).replace('.', ''))
+        print(l)
+    return l
 
 def escape_hash_tag(l: str) -> str:
     '''
     ハッシュタグをコードブロックに変換。
     '''
-    for m in re.finditer(r'#(.+?)[ \t]', ignore_code(l)):
+    for m in re.finditer(r'#([^\x01-\x7E]|\w|\.|%|&)+', ignore_code(l)):
         l = l.replace(m.group(0), '`' + m.group(0) + '`')
     if l.startswith('#'):  # 1行全てタグの場合
         l = '`' + l + '`'
@@ -124,16 +138,41 @@ def convert_link(l: str) -> str:
     リンクをMarkdownに変換。
     '''
     for m in re.finditer(r'\[(.+?)\]', ignore_code(l)):
+        is_link = True
         # タイトル+リンク形式の場合を考慮する
         tmp = m.group(1).split(' ')
-        if len(tmp) == 2:
+        if len(tmp) >= 2:
             if tmp[0].startswith('http'):
-                link, title = tmp
+                link = tmp.pop(0)
+                title = ' '.join(tmp)
+            elif tmp[-1].startswith('http'):
+                link = tmp.pop()
+                title = ' '.join(tmp)
             else:
-                title, link = tmp
-            l = l.replace(m.group(0), f'[{title}]({link})')
+                link = ''
+                title = ' '.join(tmp)
+                is_link = False
+                
+            if isGyazoLink(link):
+                links = getGyazoImageLinks(link)
+                for li in links:
+                    if isExistingImageUrl(li):
+                        link = li
+            if is_link:
+                l = l.replace(m.group(0), f'[{title}]({link})')
+            else:
+                l = l.replace(m.group(0), title)
         else:
-            l = l.replace(m.group(0), m.group(0) + '()')
+            removed = m.group(0).replace('[', '').replace(']', '')
+            if isGyazoLink(removed):
+                links = getGyazoImageLinks(removed)
+                for li in links:
+                    if isExistingImageUrl(li):
+                        l = l.replace(m.group(0), li+ '(' + li + ')') 
+                        break
+            else:
+                l = l.replace(m.group(0), removed)
+            
     return l
 
 
@@ -145,6 +184,27 @@ def ignore_code(l: str) -> str:
         l = l.replace(m.group(0), '')
     return l
 
+def getGyazoImageLinks(link: str) -> list:
+    '''
+    Gyazoのリンクを画像リンクに変換する。
+    '''
+    m = re.match(r'https://gyazo.com/(.+?)$', link)
+    if m:
+        return [f'https://i.gyazo.com/{m.group(1)}.png', f'https://i.gyazo.com/{m.group(1)}.jpg']
+    return [link]
+
+def isGyazoLink(link: str) -> bool:
+    '''
+    Gyazoのリンクかどうかを返す。
+    '''
+    return re.match(r'https://gyazo.com/.+?$', link) != None
+
+def isExistingImageUrl(link: str) -> bool:
+    '''
+    画像リンクが存在するかどうかを返す。
+    '''
+    return requests.head(link).status_code == 200
+    
 
 if __name__ == "__main__":
     main()
